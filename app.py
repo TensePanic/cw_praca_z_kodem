8ee595d3fe50c772ee7b08d97d4600c18cc90495
TensePanic <ala0712111998@gmail.com>
2023-05-12 19:04:13 +0200

    Update app.py

--- app.py
+++ app.py
@@ -7,6 +7,6 @@ app = Flask(__name__)
 @app.route('/')
 def index():
   """Function returning a greeting."""
-    return '<h1>Hello WSB! Greetings from Flask!</h1>'
+  return '<h1>Hello WSB! Greetings from Flask!</h1>'
 if __name__ == "__main__":
-    app.run(debug=True)
+  app.run(debug=True)
